e4a50b4f4fc51e9311cdf89f264441048b8b8581
Joshua Joseph <joshuadondoyq2@gmail.com>
2018-07-28 18:00:13 +0800

    task performance

---
+++ app/src/main/java/edu/sti/mylaboratory/MainLaboratory.java
@@ -0,0 +1,19 @@
+package edu.sti.mylaboratory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainLaboratory extends AppCompatActivity {
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_laboratory);
+    }
+}
